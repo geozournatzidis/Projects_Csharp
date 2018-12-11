f2251dcea1225eb7f5e9470d8b4324510e4678a8
geozournatzidis <45797874+geozournatzidis@users.noreply.github.com>
2018-12-11 20:27:33 +0200

    Add files via upload

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many times you want to print?");
+            int timesToPrint = int.Parse(Console.ReadLine());
+
+            //while (timesToPrint > 0)
+            //{
+            //    Console.WriteLine("Printing Document...");
+            //    timesToPrint = timesToPrint - 1;
+            //}
+
+            do
+            {
+                Console.WriteLine("Printing Document...");
+                timesToPrint--; // timesToPrint = timesToPrint - 1;
+            } while (timesToPrint > 0);
+
+            Console.WriteLine("End Printing.");
+        }
+    }
+}
